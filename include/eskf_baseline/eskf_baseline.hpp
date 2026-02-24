06028a916d1553db0f254b2fb9983b7b01b700b6
H S Helson Go <hellston20a@gmail.com>
2026-02-24 02:06:08 -0500

    Factor out rotation maths

--- include/eskf_baseline/eskf_baseline.hpp
+++ include/eskf_baseline/eskf_baseline.hpp
@@ -2,83 +2,9 @@
 #define ESKF_MATH_ESKF_MATH_HPP_
 
 #include "Eigen/Dense"
+#include "eskf_baseline/math.hpp"
 
 namespace eskf {
-
-namespace rotation {
-
-template <typename Derived>
-Eigen::Matrix3<typename Derived::Scalar> hat(
-    const Eigen::MatrixBase<Derived>& v) {
-  using Scalar = typename Derived::Scalar;
-  Eigen::Matrix3<Scalar> v_hat;
-  v_hat << Scalar(0), -v.z(), v.y(),  //
-      v.z(), Scalar(0), -v.x(),       //
-      -v.y(), v.x(), Scalar(0);
-  return v_hat;
-}
-
-template <typename Derived>
-Eigen::Quaternion<typename Derived::Scalar> AngleAxisToQuaternion(
-    const Eigen::MatrixBase<Derived>& angle_axis) {
-  using Scalar = typename Derived::Scalar;
-  using std::abs;
-  using std::cos;
-  using std::sin;
-  using std::sqrt;
-
-  const Scalar theta_sq = angle_axis.squaredNorm();
-
-  Scalar imag_factor;
-  Scalar real_factor;
-
-  if (theta_sq < Scalar(1e-6)) {
-    const Scalar theta_po4 = theta_sq * theta_sq;
-    imag_factor = Scalar(0.5) - Scalar(1.0 / 48.0) * theta_sq +
-                  Scalar(1.0 / 3840.0) * theta_po4;
-    real_factor = Scalar(1) - Scalar(1.0 / 8.0) * theta_sq +
-                  Scalar(1.0 / 384.0) * theta_po4;
-  } else {
-    const Scalar theta = sqrt(theta_sq);
-    const Scalar half_theta = Scalar(0.5) * theta;
-    const Scalar sin_half_theta = sin(half_theta);
-    imag_factor = sin_half_theta / theta;
-    real_factor = cos(half_theta);
-  }
-
-  Eigen::Quaternion<Scalar> quaternion;
-  quaternion.w() = real_factor;
-  quaternion.vec() = imag_factor * angle_axis;
-  return quaternion;
-}
-
-template <typename Derived>
-Eigen::Matrix3<typename Derived::Scalar> AngleAxisToRotationMatrix(
-    const Eigen::MatrixBase<Derived>& angle_axis) {
-  using std::cos;
-  using std::sin;
-  using std::sqrt;
-  using Scalar = typename Derived::Scalar;
-
-  const Scalar theta_sq = angle_axis.squaredNorm();
-  Eigen::Matrix3<Scalar> rotation_matrix = Eigen::Matrix3<Scalar>::Identity();
-  const Eigen::Matrix3<Scalar> hat_phi = hat(angle_axis);
-  const Eigen::Matrix3<Scalar> hat_phi_sq = hat_phi * hat_phi;
-
-  if (theta_sq < Scalar(1e-6)) {
-    rotation_matrix += hat_phi + hat_phi_sq / Scalar(2);
-  } else {
-    const Scalar theta = sqrt(theta_sq);
-    const Scalar cos_theta = cos(theta);
-    const Scalar sin_theta = sin(theta);
-
-    rotation_matrix += (Scalar(1) - cos_theta) / theta_sq * hat_phi_sq +
-                       sin_theta / theta * hat_phi;
-  }
-
-  return rotation_matrix;
-}
-}  // namespace rotation
 
 template <typename T>
 struct NominalState {
